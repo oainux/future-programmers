bf131c28b56cac4c6444ac81bb86d5e539d30f7e
oainux <oainux@outlook.com>
2022-09-08 19:20:00 +0300

    Solved E

--- 2017/E.py
+++ 2017/E.py
@@ -1,16 +1,22 @@
-# NOT COMPLETED
-# كنت اسويه فالحصه بس خلصت الحصه
 platforms = int(input("Enter the number of platforms: "))
+
+# make the user input again if platforms are less than 1 or greater than 300
 while platforms < 1 or platforms > 300: platforms = int(input("Enter the number of platforms: "))
+
+# input the values of the platform
 platform_values = [int(input(f"Enter the {x+1} value of your platform: ")) for x in range(platforms)]
+
+# neccessary stuff for the algorithm
 platform_values.sort()
 base = platform_values[0]
-curr_value = platform_values[0]
-poped = platform_values.pop(0)
+curr = platform_values[0]
 jumps = 0
+
+#algorithm
 for i in platform_values:
-    if i > curr_value:
-        curr_value = i
-    if curr_value <= base+60:
-        jumps+=1
+	if i > curr and i <= base+60:
+		curr = i
+	else:
+		jumps+=1
+		base = curr
 print(jumps)
